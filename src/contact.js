87a5d87357c5ee6bc9f4a30656dc2f98addf22d0
Adair <adair.reyes450@gmail.com>
2022-02-10 06:27:38 -0600

    add: Searchbar styled and searchterm state lifted up to the App Component

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,43 @@
-export default class Contact {
-  constructor(name, tNumbers = []) {
-    this.name = name;
-    this.tNumbers = tNumbers;
-  }
-}
+const accentsMap = {
+  a: "á|à|ã|â|À|Á|Ã|Â",
+  e: "é|è|ê|É|È|Ê",
+  i: "í|ì|î|Í|Ì|Î",
+  o: "ó|ò|ô|õ|Ó|Ò|Ô|Õ",
+  u: "ú|ù|û|ü|Ú|Ù|Û|Ü",
+  c: "ç|Ç",
+  n: "ñ|Ñ",
+};
+
+export const slugify = text =>
+  Object.keys(accentsMap).reduce(
+    (acc, cur) => acc.replace(new RegExp(accentsMap[cur], "g"), cur),
+    text
+  );
 
 export const contacts = [
   {
     firstName: "Isidro",
     lastName: "Manzanillotla",
+    numbers: [{ home: "1234567890" }, { fax: "1234567890" }],
+  },
+  {
+    firstName: "Mike",
+    lastName: "Wazowsky",
+    numbers: [{ home: "1234567890" }, { fax: "1234567890" }],
+  },
+  {
+    firstName: "Cockroach",
+    lastName: "Boy",
+    numbers: [{ home: "1234567890" }, { fax: "1234567890" }],
+  },
+  {
+    firstName: "Pedro",
+    lastName: "Parques",
+    numbers: [{ home: "1234567890" }, { fax: "1234567890" }],
+  },
+  {
+    firstName: "Perrito",
+    lastName: "Loquito",
     numbers: [{ home: "1234567890" }, { fax: "1234567890" }],
   },
   {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
-import { contacts } from "./contact";
+import { contacts, slugify } from "./contact";
 
-function App() {
-  return (
-    <>
-      <SearchBar />
-      <ContactsContainer contacts={contacts} />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+    };
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    const term = e.target.value;
+    this.setState({ searchTerm: term });
+  }
+
+  render() {
+    const term = this.state.searchTerm.toLowerCase().trim();
+    let searchDialog;
+    if (term) {
+      // contacts that matched the search query
+      const match = this.props.contacts.filter(contact => {
+        const fullNameTerm = `${contact.firstName} ${contact.lastName}`;
+        if (slugify(fullNameTerm.toLowerCase()).indexOf(term) !== -1) {
+          return contact;
+        }
+      });
+      searchDialog = <SearchDialog contacts={match}></SearchDialog>;
+    }
+    return (
+      <div className="contact-app">
+        <SearchBar term={this.state.searchTerm} onSearch={this.handleSearch} />
+        {searchDialog}
+        <ContactsContainer contacts={this.props.contacts} />
+      </div>
+    );
+  }
+}
+
+function SearchDialog(props) {
+  if (!props.contacts) return null;
+
+  const contacts = props.contacts.map(contact => {
+    return <Contact {...contact} key={contact.name + contact.lastName} />;
+  });
+  return <div className="search-dialog">{contacts}</div>;
 }
 
 function sortContactsAlphabetically(contacts) {
@@ -38,25 +76,23 @@ function divideOnGroups(contacts) {
 }
 
 function ContactsContainer(props) {
-  // Here we need to generate the different groups
-
   const sorted = sortContactsAlphabetically(props.contacts);
   const groups = divideOnGroups(sorted).map(group => {
     return <ContactGroup contacts={group} key={group[0].firstName.charAt(0)} />;
   });
 
-  // const groups = [<ContactGroup contacts={props.contacts} key={"first"} />];
-
   return <div className="contacts-container">{groups}</div>;
 }
 
 function ContactGroup(props) {
+  const groupLetter = props.contacts[0].firstName.charAt(0);
+
   const contacts = props.contacts.map(contact => {
     return <Contact {...contact} key={contact.name + contact.lastName} />;
   });
   return (
     <div className="contact-group">
-      <p className="contact-index">{props.index}</p>
+      <p className="contact-index">{groupLetter}</p>
       {contacts}
     </div>
   );
@@ -85,19 +121,23 @@ function ThreeDotButton() {
   );
 }
 
-function SearchBar() {
+function SearchBar(props) {
   return (
     <div className="search-container">
+      {/* <label htmlFor="search-bar">Search</label> */}
       <input
+        id="search-bar"
         className="search-input"
+        value={props.term}
+        onChange={props.onSearch}
         placeholder="Search contact name"
         type="text"
       />
-      <button className="search-button" type="submit">
+      {/* <button className="search-button" type="submit">
         Search
-      </button>
+      </button> */}
     </div>
   );
 }
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<App contacts={contacts} />, document.getElementById("root"));
